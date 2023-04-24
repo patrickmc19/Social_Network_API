b48330e700049c9670a26347d6f3de45916b420c
Patrick McKnight <patrickgmcknight@gmail.com>
2023-04-24 16:18:01 -0700

    created User model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const opt = {
+  toJSON: {
+    virtuals: true,
+    getters: true,
+    id: false,
+  },
+};
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email address is required"],
+    validate: {
+      validator: function (v) {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+}, opt );
+
+const User = model("User", UserSchema);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = User;
